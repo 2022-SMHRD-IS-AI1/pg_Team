8ab277cf41bcd46d35e785ef8f5094bf176c378b
smhrd <smhrd@DESKTOP-U1H45EU>
2022-12-26 10:58:27 +0900

    fdfd

---
+++ Git_Test/src/main/java/test_code/Test.java
@@ -0,0 +1,17 @@
+package test_code;
+
+import model.PG_DAO;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		PG_DAO dao = new PG_DAO();
+		
+		dao.getConn();
+		dao.getConn();
+		
+	}
+
+}
